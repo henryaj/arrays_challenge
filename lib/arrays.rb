2943e83044ecd2afad42747bc9d6148a607a53f9
Henry Stanley <henry@henrystanley.com>
2014-09-05 10:46:54 +0100

    updated tests to compare with Ruby built-in inject method; added method that checks to see if the user has passed in a symbol and converts it to a symbol

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,11 +1,26 @@
 class Array
 
-	def injector(start, &block)
-		mem = start
-		self.each do |number|
-			yield mem, number
-			mem = mem
+	def injector(arg, &block)
+		
+		if !block_given? # assume that arg is an operator	
+			mem = self.first
+			self.each_with_index do |number, index|
+				if index == 0
+					next # skips the first number of the array
+				else
+					a = arg.to_proc # turns the symbol given into a proc, gets called on the next line
+					mem = a.call(mem, number)
+				end
+			end
+		
+		else
+			mem = arg
+			self.each do |number|
+				mem = yield mem, number
+			end
+		
 		end
+		
 		mem
 	end
 
--- spec/arrays_spec.rb
+++ spec/arrays_spec.rb
@@ -4,23 +4,51 @@ describe Array do
 
 	describe 'injector' do
 
-		let(:array) { [1,2,3] }
+		let(:array) { [1,2,3,4,5] }
+
+		context 'using blocks' do
 
 		it 'can be called as a method on an array' do
 			expect(array.methods).to include(:injector)
 		end
 
-		it 'when given a block with a +, should add the values and iterate' do
-			expect(array.injector(0) { |mem,x| mem + x } ).to eq 6
+		it 'when given a block with +, should add the values and iterate' do
+			expect(array.injector(0) { |mem,x| mem + x } ).to eq(array.inject(0) { |mem,x| mem + x } )
 		end
 
 		it 'when given a block with *, should multiply each value with mem and give the result' do
-			expect(array.injector(1) { |mem,x| mem * x } ).to eq 6
+			expect(array.injector(1) { |mem,x| mem * x } ).to eq(array.inject(1) { |mem,x| mem * x } )
 		end
 
 		it 'when given a block with -, should subtract each value with mem and give the result' do
-			expect(array.injector(1) { |mem,x| mem * x } ).to eq 6
+			expect(array.injector(0) { |mem,x| mem - x } ).to eq(array.inject(0) { |mem,x| mem - x } )
 		end
+
+		it 'when given a block with /, should divide each value with mem and give the result' do
+			expect(array.injector(0) { |mem,x| mem / x } ).to eq(array.inject(0) { |mem,x| mem / x } )
+		end
+
+	end
+
+	context 'using symbols' do
+
+		it 'when given :+ as an argument, should add' do
+			expect(array.injector(:+)).to eq(array.inject(:+))
+		end
+
+		it 'when given :- as an argument, should subtract' do
+			expect(array.injector(:-)).to eq(array.inject(:-))
+		end
+
+		it 'when given :* as an argument, should multiply' do
+			expect(array.injector(:*)).to eq(array.inject(:*))
+		end
+
+		it 'when given :/ as an argument, should divide' do
+			expect(array.injector(:/)).to eq(array.inject(:/))
+		end
+
+	end
 
 	end
 
