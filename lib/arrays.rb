c757533f00c895964ec3711f0ce4db53d5ba14c8
Henry Stanley <henry@henrystanley.com>
2014-10-31 14:50:36 +0000

    Reformatted code to use 2 spaces instead of tabs

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,27 +1,27 @@
 class Array
 
-	def injector(arg, &block)
-		
-		if !block_given? # assume that arg is an operator	
-			mem = self.first
-			self.each_with_index do |number, index|
-				if index == 0
-					next # skips the first number of the array
-				else
-					a = arg.to_proc # turns the symbol given into a proc, gets called on the next line
-					mem = a.call(mem, number)
-				end
-			end
-		
-		else
-			mem = arg
-			self.each do |number|
-				mem = yield mem, number
-			end
-		
-		end
-		
-		mem
-	end
+  def injector(arg, &block)
+    
+    if !block_given? # assume that arg is an operator 
+      mem = self.first
+      self.each_with_index do |number, index|
+        if index == 0
+          next # skips the first number of the array
+        else
+          a = arg.to_proc # turns the symbol given into a proc, gets called on the next line
+          mem = a.call(mem, number)
+        end
+      end
+    
+    else
+      mem = arg
+      self.each do |number|
+        mem = yield mem, number
+      end
+    
+    end
+    
+    mem
+  end
 
 end
