287b24172fd0689c898555b96de115a128354f8b
Henry Stanley <henry@henrystanley.com>
2014-10-31 14:56:19 +0000

    Refactored arrays method significantly

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,27 +1,13 @@
 class Array
 
-	def injector(arg, &block)
-		
-		if !block_given? # assume that arg is an operator	
-			mem = self.first
-			self.each_with_index do |number, index|
-				if index == 0
-					next # skips the first number of the array
-				else
-					a = arg.to_proc # turns the symbol given into a proc, gets called on the next line
-					mem = a.call(mem, number)
-				end
-			end
-		
-		else
-			mem = arg
-			self.each do |number|
-				mem = yield mem, number
-			end
-		
+	def injector(arg)	
+		copy = self.dup
+		mem = arg.is_a?(Symbol) ? copy.shift : arg
+		if block_given? 
+			copy.each { |number| mem = yield(mem, number) }
+		else	
+			copy.each { |number| mem = mem.send(arg, number) }
 		end
-		
 		mem
 	end
-
 end
